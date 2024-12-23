1c9f859a76eeeb940dd4457cd2ba7a6fa0264d71
Yousif Kndkji <yousif.kndkji@gmail.com>
2024-12-23 06:46:29 -0500

    add check to only pass events to extension if extension UI is open

--- src/scripts/service-worker.js
+++ src/scripts/service-worker.js
@@ -1,20 +1,29 @@
-chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
+function buildTabData(tab) {
+	// see if tab.windowId is useful
+	return {
+		id: tab.id,
+		title: tab.title,
+		url: tab.url,
+		active: tab.active,
+		discarded: tab.discarded,
+		icon: tab.favIconUrl,
+		windowId: tab.windowId,
+	};
+}
+
+function buildWindowData(window) {
+	return {
+		id: window.id,
+		tabs: window.tabs.map(tab => buildTabData(tab)),
+		incognito: window.incognito,
+		focused: window.focused,
+	};
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message.type === "GET_ALL_WINDOWS") {
 		chrome.windows.getAll({ populate: true }, windows => {
-			// see if including window.id in tab data is useful (unlikely)
-			const windowsData = windows.map(window => ({
-				id: window.id,
-				tabs: window.tabs.map(tab => ({
-					id: tab.id,
-					title: tab.title,
-					url: tab.url,
-					discarded: tab.discarded,
-					icon: tab.favIconUrl,
-					windowId: tab.windowId,
-				})),
-				incognito: window.incognito,
-				focused: window.focused,
-			}));
+			const windowsData = windows.map(window => buildWindowData(window));
 			console.log("sending all tabs:", windowsData);
 			sendResponse(windowsData);
 		});
@@ -25,46 +34,44 @@ chrome.runtime.onMessage.addListener((message, _, sendResponse) => {
 /*
  * tab.discarded
  * tab.windowId
- * will onUpdated also fire if onRemoved fires
+ * will onUpdated also fire if onRemoved fires: No
  * check if onDetached will not fire so long as user detaches it and continues moving it (i.e., they might attach it to another window)
  * maybe debounce the onDetached if possible lol...
+ * maybe use only onUpdated instead of onCreated
+ * figure out a way to make service worker know when user creates a new tab/window or if the extension created a new tab/window
  */
-
-let newTabCompare = [];
 
 chrome.tabs.onCreated.addListener(tab => {
 	console.log("from tabs created: ", tab);
-	newTabCompare.push(tab);
-	chrome.runtime.sendMessage({ type: "TAB_CREATED", tab });
+	chrome.runtime.getContexts({ contextTypes: ["POPUP", "SIDE_PANEL"] }).then(contexts => {
+		console.log("found context? ", contexts.length);
+		if (contexts.length > 0) {
+			tab.chrome.runtime.sendMessage({ type: "TAB_CREATE", tab: buildTabData(tab) });
+		}
+	});
 });
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
 	console.log("from tabs removed: ", tabId, removeInfo);
-	console.log("isequal: ");
-	for (let i = 0; i < 3; i++) {
-		console.log(newTabCompare[i]);
-	}
-
-	const prevInfo = newTabCompare[1];
-	for (const [k, v] of Object.entries(newTabCompare[2])) {
-		if (typeof prevInfo[k] === "object") {
-			for (const [k2, v2] of Object.entries(newTabCompare[2][k])) {
-				if (prevInfo[k][k2] !== v2) {
-					console.log("deep diff", k, "(", k2, ")", ":", prevInfo[k][k2], v2);
-				}
+	if (!removeInfo.isWindowClosing) {
+		chrome.runtime.getContexts({ contextTypes: ["POPUP", "SIDE_PANEL"] }).then(contexts => {
+			console.log("found context? ", contexts.length);
+			if (contexts.length > 0) {
+				chrome.runtime.sendMessage({ type: "TAB_DELETE", tabId, windowId: removeInfo.windowId });
 			}
-		} else if (prevInfo[k] !== v) {
-			console.log("diff", k, ":", prevInfo[k], v);
-		}
+		});
 	}
-	console.log(newTabCompare[1] === newTabCompare[2]);
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	console.log("from tabs updated: ", tabId, changeInfo);
-	if (changeInfo.url || changeInfo.discarded) {
-		newTabCompare.push(tab);
-		chrome.runtime.sendMessage({ type: "TAB_UPDATED", tabId, tab });
+	console.log("from tabs updated: ", tabId, changeInfo, tab);
+	if (changeInfo.status === "complete" || changeInfo.favIconUrl || changeInfo.discarded) {
+		chrome.runtime.getContexts({ contextTypes: ["POPUP", "SIDE_PANEL"] }).then(contexts => {
+			console.log("found context? ", contexts.length);
+			if (contexts.length > 0) {
+				chrome.runtime.sendMessage({ type: "TAB_UPDATE", tabId, changeInfo, tab: buildTabData(tab) });
+			}
+		});
 	}
 });
 
